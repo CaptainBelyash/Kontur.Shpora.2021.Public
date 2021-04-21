b65ab3d5e4319a0db61a4d007d2e7ddee6dadc44
ilya.belyaev.student@gmail.com <50026308+CaptainBelyash@users.noreply.github.com>
2021-04-21 21:58:16 +0500

    Change to Monitors

--- ReaderWriterLock/RwLock.cs
+++ ReaderWriterLock/RwLock.cs
@@ -7,27 +7,30 @@ namespace ReaderWriterLock
     public class RwLock : IRwLock
     {
         private object writerLocker = new();
-        private HashSet<Mutex> riderMutexes = new();
+        private HashSet<object> riderLockers = new();
         
         public void ReadLocked(Action action)
         {
-            var mutex = new Mutex();
-            lock (writerLocker)
+            var riderLocker = new object();
+            
+            lock (riderLocker)
             {
-                riderMutexes.Add(mutex);
-                mutex.WaitOne();
+                lock (writerLocker)
+                {
+                    riderLockers.Add(riderLocker);
+                }
+                action();
             }
-            action();
-            mutex.ReleaseMutex();
         }
 
         public void WriteLocked(Action action)
         {
             lock (writerLocker)
             {
-                riderMutexes.ForEach(m => m.WaitOne());
+                riderLockers.ForEach(locker => Monitor.Enter(locker));
                 action();
-                riderMutexes.Clear();
+                riderLockers.ForEach(locker => Monitor.Exit(locker));
+                riderLockers.Clear();
             }
         }
     }
