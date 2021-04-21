0b812c839891e5a441c11407f9b5aa92f3f9a216
ilya.belyaev.student@gmail.com <50026308+CaptainBelyash@users.noreply.github.com>
2021-04-21 20:10:42 +0500

    My RWLocker

---
+++ ReaderWriterLock/RwLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ReaderWriterLock
+{
+    public class RwLock : IRwLock
+    {
+        private object writerLocker = new();
+        private HashSet<Mutex> riderMutexes = new();
+        
+        public void ReadLocked(Action action)
+        {
+            var mutex = new Mutex();
+            lock (writerLocker)
+            {
+                riderMutexes.Add(mutex);
+                mutex.WaitOne();
+            }
+            action();
+            mutex.ReleaseMutex();
+        }
+
+        public void WriteLocked(Action action)
+        {
+            lock (writerLocker)
+            {
+                riderMutexes.ForEach(m => m.WaitOne());
+                action();
+                riderMutexes.Clear();
+            }
+        }
+    }
+}
